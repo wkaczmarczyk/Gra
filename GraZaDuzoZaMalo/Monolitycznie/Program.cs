becd66b533c0f693114b435dfa4d9aa925247ff1
wkaczmarczyk <55107797+wkaczmarczyk@users.noreply.github.com>
2019-09-11 20:25:17 +0200

    Wersja1

---
+++ GraZaDuzoZaMalo/Monolitycznie/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monolitycznie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. komputer losuje liczbę z podanego zakresu
+            Random los = new Random();
+            int wylosowana = los.Next(1, 101);
+#if DEBUG
+            Console.WriteLine(wylosowana); //do usuniecia w Relase
+#endif
+
+            //powtarzaj wielokrotnie az odgadnie 
+            //2. człowiek proponuje (odgaduje)
+            
+            //3. komputer ocenia poropozycję
+            //koniec powtarzaj
+            
+            //4. wypisz statystyki gry 
+        }
+    }
+}
